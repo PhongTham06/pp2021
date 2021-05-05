859bc30123610d29c6498014692b49e9750a6f33
PhongTham <phongcga3@gmail.com>
2021-05-05 20:03:05 +0700

    Update for pw7 (not finished the redirection)

--- 6.shell.py
+++ 6.shell.py
@@ -1,8 +1,26 @@
-import os
+import subprocess
 
 while True:
     command = input("shell_test>>>")
     if command == "exit":
         break
     else:
-        os.system(command)
+        if ">" in command:
+            # not finished
+            pass
+            command_check = command.split(">")
+        elif "<" in command:
+            # not finished
+            pass
+            command_check = command.split("<")
+            
+        elif "|" in command:
+            # not finished
+            pass
+            command_check = command.split("|")
+        else:
+            command_check = command.split()
+            if len(command_check) == 1:
+                subprocess.run(command)
+            elif len(command_check) == 2:
+                subprocess.run([command_check[0], command_check[1]])
